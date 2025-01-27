66d0266ba3109c986976684d85f23de4e62e2b80
hayllonprata <102913136+hayllonprata@users.noreply.github.com>
2025-01-27 14:55:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,13 @@ require_once 'functions.php';
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="theme-color" content="#000000">
+
     <title>Zordin - Dashboard Financeiro</title>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <link href="style.css" rel="stylesheet">
 </head>
 <body>
