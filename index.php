02bbdcb1c249412b949c278c60da0f21d8100bb3
hayllonprata <102913136+hayllonprata@users.noreply.github.com>
2025-01-27 22:41:03 -0300

    update

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require_once 'functions.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="theme-color" content="#000000">
 
-    <title>Zordin - Vida Financeira</title>
+    <title>Zordin - Dinheiro com Inteligência</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
 
